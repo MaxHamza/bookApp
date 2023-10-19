a7a7be795560def7f65901e1b20d306b7a3f04f8
Hamza <hamza1mks1@gmail.com>
2023-10-19 10:20:36 +0300

    finish book entity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity{
+  final String image;
+  final String author;
+  final String title;
+  final num price;
+  final num rating;
+  BookEntity({required this.image,required this.author,required this.title,required this.price,required this.rating});
+
+}
